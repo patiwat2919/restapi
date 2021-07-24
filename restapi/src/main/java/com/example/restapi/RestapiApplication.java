65970cd61be29cb0be6f695e7e3b0bf74c338d6e
patiwat.kam <patiwat.kam@turbo.co.th>
2021-07-24 08:52:11 +0000

    001

--- restapi/src/main/java/com/example/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/example/restapi/RestapiApplication.java
@@ -2,12 +2,22 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class RestapiApplication {
 
+	@Bean
+	public Random createRandom() {
+		return new Random();
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(RestapiApplication.class, args);
+		ConfigurableApplicationContext ctx
+				= SpringApplication.run(RestapiApplication.class, args);
 	}
 
 }
